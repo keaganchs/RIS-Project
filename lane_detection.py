bd8020ca69ee1bd33574bb150779b844c76e80dc
Eunchong Kim <dmschd12354@github.com>
2022-04-22 18:29:34 +0200

    pub_sub_modified

--- lane_detection.py
+++ lane_detection.py
@@ -8,38 +8,36 @@ import numpy
 import rospy
 from sensor_msgs.msg import Image
 
-cv2.imread()
-
 class GetImage(object):
     def __init__(self):
         # Update frequency 
-        self.freq   = rospy.Rate(1)
-        self.image  = None
+        self.freq = rospy.Rate(1)
         self.bridge = CvBridge()
 
         # Publisher
         self.pub = rospy.Publisher('imagetimer', Image, queue_size=10)
 
-        # Subscriber
-        rospy.Subscriber("/tesla_roadster/camera_node/image/raw", Image, self.callback)
+    def line_detection(self, image):
+        # line detection code
+        self.pub.publish(self.bridge.cv2_to_imgmsg(image)) 
 
     def callback(self, msg):
         rospy.loginfo('Got image')
-        self.image = self.bridge.imgmsg_to_cv2(msg)
+        image = self.bridge.imgmsg_to_cv2(msg)
+        self.line_detection(image)
 
     def start(self):
         rospy.loginfo('Reading images...')
         while not rospy.is_shutdown():
             rospy.logifo('Publishing image')
             if self.image is not None:
-                self.pub.publish(self.bridge.cv2_to_imgmsg(self.image))
+                rospy.Subscriber("/tesla_roadster/camera_node/image/raw", Image, self.callback)
             self.freq.sleep()
 
 if __name__ == '__main__':
     rospy.init_node("imagetimer111", anonymous=True)
     getImageNode = GetImage()
     getImageNode.start()
-
 
 
 
