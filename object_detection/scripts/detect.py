daf7bfc23cf36e1803325bfb0c4c109cc582966b
Eunchong Kim <dmschd12354@github.com>
2022-05-29 19:46:28 +0200

    FINAL

--- object_detection/scripts/detect.py
+++ object_detection/scripts/detect.py
@@ -17,13 +17,7 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 def detect(opt, save_img=False):
     source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    #webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        #('rtsp://', 'rtmp://', 'http://'))
-
-    # Directories
-    #save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-#     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
+  
     # Initialize
     set_logging()
     device = select_device(opt.device)
@@ -44,12 +38,7 @@ def detect(opt, save_img=False):
 
     # Set Dataloader
     vid_path, vid_writer = None, None
-    #if webcam:
-       # view_img = check_imshow()
-       # cudnn.benchmark = True  # set True to speed up constant image size inference
-       # dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    #else:
-    #save_img = True
+    
     dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
@@ -81,67 +70,15 @@ def detect(opt, save_img=False):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            #if webcam:  # batch_size >= 1
-                #p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
-            #else:
             p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
-
-            #p = Path(p)  # to Path
-            #save_path = str(save_dir / p.name)  # img.jpg
-            #txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            #s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-                # Print results
-                #for c in det[:, -1].unique():
-                    #n = (det[:, -1] == c).sum()  # detections per class
-                    #s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    #if save_txt:  # Write to file
-                        #xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        #line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        #with open(txt_path + '.txt', 'a') as f:
-                        #    f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                #if save_img or view_img:  # Add bbox to image
                     label = f'{names[int(cls)]} {conf:.2f}'
                     plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-            # Print time (inference + NMS)
-            #print(f'{s}Done. ({t2 - t1:.3f}s)')
-
-            # Stream results
-            #if view_img:
-                #cv2.imshow(str(p), im0)
-                #cv2.waitKey(1)  # 1 millisecond
         
             return im0
-
-#             # Save results (image with detections)
-#             if save_img:
-#                 if dataset.mode == 'image':
-#                     cv2.imwrite(save_path, im0)
-#                 else:  # 'video'
-#                     if vid_path != save_path:  # new video
-#                         vid_path = save_path
-#                         if isinstance(vid_writer, cv2.VideoWriter):
-#                             vid_writer.release()  # release previous video writer
-
-#                         fourcc = 'mp4v'  # output video codec
-#                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
-#                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-#                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-#                     vid_writer.write(im0)
-
-#     if save_txt or save_img:
-#         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-#         print(f"Results saved to {save_dir}{s}")
-
-#     print(f'Done. ({time.time() - t0:.3f}s)')
-#     return f"{save_dir}{s}"
